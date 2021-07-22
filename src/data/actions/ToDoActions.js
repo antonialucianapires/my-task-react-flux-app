90d20b9c817c083729f151f86bdc039f0157830f
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-07-21 23:20:52 -0300

    feat: criaçao das actions + dispatcher

---
+++ src/data/actions/ToDoActions.js
@@ -0,0 +1,31 @@
+import AppDispacher from "../dispatcher/AppDispatcher";
+import TodoConstants from "../constants/ToDoConstant";
+
+const TodoActions = {
+  create(description) {
+    AppDispacher.dispatch({
+      actionType: TodoConstants.TODO_CREATE,
+      description: description,
+    });
+  },
+
+  update(item) {
+    AppDispacher.dispatch({
+      actionType: TodoConstants.TODO_UPDATE,
+      item,
+    });
+  },
+  remove(id) {
+    AppDispacher.dispatch({
+      actionType: TodoConstants.TODO_REMOVE,
+      id,
+    });
+  },
+  clear() {
+    AppDispacher.dispatch({
+      actionType: TodoConstants.TODO_CLEAR,
+    });
+  },
+};
+
+export default TodoActions;
