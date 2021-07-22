8fd80aca2921652c41754710cede1e4bd456a7ed
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-07-21 23:29:43 -0300

    feat: store - dados e funçoes

---
+++ src/data/stores/TodoStore.js
@@ -0,0 +1,41 @@
+import { TodoService } from "./data/services/TodoService";
+
+let _todoList = [];
+
+function createItem(description) {
+  return TodoService.create({
+    description,
+    isChecked: false,
+  }).then((newItem) => {
+    _todoList.push(newItem);
+  });
+}
+
+function updateItem(newItem) {
+  const itemIndex = _todoList.findIndex((item) => item.id === newItem.id);
+
+  _todoList[itemIndex] = newItem;
+  return TodoService.update(newItem);
+}
+
+function removeItem(id) {
+  const itemIndex = _todoList.findIndex((item) => item.id === id);
+  _todoList.splice(itemIndex, 1);
+  return TodoService.remove(id);
+}
+
+function clearAll() {
+  const todo = [],
+    done = [];
+
+  _todoList.forEach((item) => {
+    if (item.isChecked) {
+      done.push(item);
+    } else {
+      todo.push(item);
+    }
+  });
+
+  done.forEach((item) => this.removeItem(item.id));
+  _todoList = todo;
+}
